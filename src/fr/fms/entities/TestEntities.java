41c8fac1981cd8f9244152caac0989b8c34a195b
Severine Diribarne <severine.diribarne@fms-ea.com>
2026-08-19 13:40:49 +0200

    Display information about employees and salesperson contained in a list

--- src/fr/fms/entities/TestEntities.java
+++ src/fr/fms/entities/TestEntities.java
@@ -1,17 +1,29 @@
 package fr.fms.entities;
 
+import java.util.ArrayList;
+import java.util.List;
+
 public class TestEntities {
 
 	public static void main(String[] args) {
 		
-		int size = 3;
-		Person tabEmployee [] = new Person[size];
-		tabEmployee[0] = new Employee("Gates", "Bill", 65,"aux USA", new City("Washington", "USA", 7600000), new Capital(null, null, 7600000, "W-M"), "Fondation B&M", 100000.0);
-		tabEmployee[1] = new Employee("Musk", "Elon", 49,"à Palo Alto", new City("Pretoria", "Afrique du Sud", 800000), new Capital(null, null, 800000, null), "Tesla", 150000.0);
-		tabEmployee[2] = new Commercial("Dupont", "robert", 50,"rue des rosiers à Toulouse", new City("Limoges", "France", 133000), new Employee(null, null, 0, null, null, null, "brico", 0), 5.0);
-
-		for (int i = 0; i< tabEmployee.length; i++) {
-			System.out.println(tabEmployee[i]);
+//		int size = 3;
+//		Person tabEmployee [] = new Person[size];
+//		tabEmployee[0] = new Employee("Gates", "Bill", 65,"aux USA", new City("Washington", "USA", 7600000), new Capital(null, null, 7600000, "W-M"), "Fondation B&M", 100000.0);
+//		tabEmployee[1] = new Employee("Musk", "Elon", 49,"à Palo Alto", new City("Pretoria", "Afrique du Sud", 800000), new Capital(null, null, 800000, null), "Tesla", 150000.0);
+//		tabEmployee[2] = new Commercial("Dupont", "robert", 50,"rue des rosiers à Toulouse", new City("Limoges", "France", 133000), new Employee(null, null, 0, null, null, null, "brico", 0), 5.0);
+//
+//		for (int i = 0; i< tabEmployee.length; i++) {
+//			System.out.println(tabEmployee[i]);
+//		}
+		
+		List<Person> persons = new ArrayList<Person>();
+		persons.add(new Employee("Gates", "Bill", 65,"aux USA", new City("Washington", "USA", 7600000), new Capital(null, null, 7600000, "W-M"), "Fondation B&M", 100000.0));
+		persons.add(new Employee("Musk", "Elon", 49,"à Palo Alto", new City("Pretoria", "Afrique du Sud", 800000), new Capital(null, null, 800000, null), "Tesla", 150000.0));
+		persons.add(new Commercial("Dupont", "robert", 50,"rue des rosiers à Toulouse", new City("Limoges", "France", 133000), new Employee(null, null, 0, null, null, null, "brico", 0), 5.0));
+	
+		for(Person person : persons) {
+			System.out.println(person);
 		}
 	}
 }
